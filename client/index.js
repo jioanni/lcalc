f6a93b4b90a14571e5fc1a45b7b3595b3d52f7a2
Jason Ioannides <jason.ioannides@gmail.com>
2018-02-21 12:44:05 -0500

    functionalish boilerplate code

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import store from './store'
+
+ReactDom.render(
+    <Provider store = {store}>
+        <Routes />
+    </Provider>,
+    document.getElementById('app')
+)
